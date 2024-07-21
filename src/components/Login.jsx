7408a81e7c5282de0e20d6112ee716927dac906d
Roslina <129685597+Roslina0110@users.noreply.github.com>
2024-07-21 22:19:57 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,62 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import axios from 'axios'
 
 const Login = () => {
   const sendData = () => {
-    console.log(form);
+    console.log('Send data');
   };
 
   return (
     <Box
-      component="form"
       sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        height: '50vh',
+        backgroundColor: '#f0f0f0', // Optional: Background color for the page
       }}
-      noValidate
-      autoComplete="off"
     >
-      <Stack spacing={2} direction="column" alignItems="center">
-        <TextField
-        required
-          id="outline-required"
-          label="Required - Username"
-          color="success"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-        required
-          id="outline-required"
-          label="Required - Password"
-          color="success"
-          variant="outlined"
-        />
-        <br />
-        <Button variant="contained" color="success" onClick={sendData}>
-          Login
-        </Button>
-      </Stack>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          backgroundColor: 'white', // White background color
+          padding: '32px', // Padding inside the box
+          borderRadius: '8px', // Rounded corners
+          boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)', // Box shadow for better visibility
+          border: '1px solid grey', // Border around the box,
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Stack spacing={2} direction="column" alignItems="center">
+          <TextField
+            required
+            id="outline-required"
+            label="Required - Username"
+            color="success"
+            variant="outlined"
+          />
+          <TextField
+            required
+            id="outline-required"
+            label="Required - Password"
+            color="success"
+            variant="outlined"
+            type="password"
+          />
+          <Button variant="contained" color="success" onClick={sendData}>
+            Login
+          </Button>
+        </Stack>
+      </Box>
     </Box>
   );
 };
