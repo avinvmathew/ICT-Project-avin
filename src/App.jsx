b0af0c99344534702f945eebcfd743321a7f4ea7
sanal <sanalsajan916@gmail.com>
2024-07-24 20:12:13 +0530

    Git

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import './App.css'
-import Login from './components/Login'
-import Navbar from './components/Navbar'
-import Signup from './components/Signup'
-import {Routes,Route} from 'react-router-dom'                   
+import { useState } from 'react';
+// import Login from './components/LoginPage/Login';
+// import Navbar from './components/LoginPage/Navbar';
+// import Signup from './components/LoginPage/Signup';
+// import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import HomeBar from './components/HomePage/HomeBar';
+import axios from 'axios';
+
 
 function App() {
- 
+  const [count, setCount] = useState(0);
+  // const navigate = useNavigate();
+
+  // const handleLogin = () => {
+  //   navigate('/home'); // navigate to /home route
+  // };
+
   return (
     <>
-      <h1>Event Management Software</h1>
-      <Navbar/>
-      <br/>
+    <HomeBar />
+      {/* <Navbar />
       <Routes>
         <Route path="/" element={<Login onLogin={handleLogin} />} />
         <Route path="/Signup" element={<Signup />} />
@@ -20,4 +29,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
